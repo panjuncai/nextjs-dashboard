c967b7c29883af7381266efdfc9a6e890600bdf7
Siqian <liu00638@algonquinlive.com>
2024-11-12 18:41:38 +0800

    init

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,5 +1,6 @@
 import { GlobeAltIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
+import Image from 'next/image'
 
 export default function AcmeLogo() {
   return (
